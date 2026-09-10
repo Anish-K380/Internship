2a00d71a752253eb9baaea3feab562a3c79a9396
Anish <anishkumar.s2005@gmail.com>
2026-09-10 10:57:15 +0530

    removed unused files

--- Securden/agent-work-flow-chart/temp/test/parse.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('samplelinks.txt', 'r') as file:
-    raw = file.read()
-
-data = eval(raw)
-
-with open('samplelinks.json', 'w') as file:
-    json.dump(data, file, indent = 2)
